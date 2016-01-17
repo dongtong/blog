b3e0e23c12d000e42b5ccd7cb7943af37ba65fb0
dongtong <jinhu.tung@gmail.com>
2016-01-17 13:09:19 +0800

    update

--- riot/riot_learning/src/js/app.js
+++ riot/riot_learning/src/js/app.js
@@ -4,8 +4,4 @@ import Riot from 'riot';
 import '../tags/hello-world.tag';
 import '../tags/greet-form.tag';
 
-// var Riot = require('riot');
-// var helloWorld = require('../tags/hello-world.tag');
-// var helloWorld = require('../tags/greet-form.tag');
-
 Riot.mount('*');
