4b21ca45446262bfb5f6a0901217c47c1f4d5327
dongtong <jinhu.tung@gmail.com>
2016-05-13 11:53:19 +0800

    event, smart and dumb component, two-way binding

--- react/learning_react/src/js/components/header.js
+++ react/learning_react/src/js/components/header.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Title from './header/title';
 
+// Dumb Component
+// 可以重用
+// 不关心状态，只关心Props
 export default class Header extends React.Component {
+	// synthetic event
+	handleChange (e) {
+		const value = e.target.value;
+		this.props.changeTitle(value);
+	}
+
+	// 双向绑定（value={}）
 	render () {
 		return (
-			<Title title={this.props.title}/>
+			<div>
+				<Title title={this.props.title} />
+				<input value={this.props.title} onChange={this.handleChange.bind(this)}/>
+			</div>
 		);
 	}
 }
--- react/learning_react/src/js/components/layout.js
+++ react/learning_react/src/js/components/layout.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
+// Smart Component
+// 集中管理State
+// 通过props将data传递給Dumb Component
+// Dumb Component 可以最大化重用
 export default class Layout extends React.Component {
+	// 把状态放在一个集中的地方管理，是一种好的实践
+	// 可以做出好的Dumb Component(Header, Footer)
 	constructor() {
 		super();
 		this.state = {
@@ -13,6 +19,12 @@ export default class Layout extends React.Component {
 
 	getGreeting () {
 		return "React";
+	}
+
+    // .bind(this)
+    // Dumb Component调用此方法时都会查找到Smart Component(this)
+	changeTitle (title) {
+		this.setState({title}); //ES6语法
 	}
 
 	render() {
@@ -26,15 +38,17 @@ export default class Layout extends React.Component {
 		// 仅仅是h3 dom刷新
 		// 刷新状态改变最近的一个DOM
 		// 打开chrome Console > Rendering > Enable paint flashing
-		setTimeout(() => {
-			this.setState({
-				title: "Welcome, Fish!"
-			});
-		}, 2000);
+		// setTimeout(() => {
+		//	this.setState({
+		//		title: "Welcome, Fish!"
+		//	});
+		//}, 2000);
 
 		return (
 			<div>
-				<Header title={this.state.title}/>
+				<Header 
+				    title = {this.state.title}
+				    changeTitle = {this.changeTitle.bind(this)} />
 				<h3>{this.state.name} say: Hello {this.getGreeting()}!</h3>
 				<ul>
 					{list}
