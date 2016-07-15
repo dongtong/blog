2e74828d6b199ce364a5da32362c559b12cd8942
jinhu.tung <jinhu.tung@gmail.com>
2016-07-16 00:42:35 +0800

    angular 2 with webpack

--- angular2/angular-starter/src/app/app.component.ts
+++ angular2/angular-starter/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My App</h1>' 
+    template: '<h1>Angular 2 with Webpack</h1>' 
 })
 
 export class AppComponent {
