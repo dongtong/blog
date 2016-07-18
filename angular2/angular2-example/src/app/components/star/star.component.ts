f059acd09ac6d82e5513516d1479702f59e7ec26
jinhu.tung <jinhu.tung@gmail.com>
2016-07-18 22:33:40 +0800

    building a nested component

---
+++ angular2/angular2-example/src/app/components/star/star.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnChanges } from '@angular2/core';
+
+@Component({
+  selector: 'ai-star',
+  templateUrl: 'src/app/templates/star/star.component.html',
+  styleUrls: ['src/app/stylesheets/star.component.css']
+})
+
+export class StarComponent implements OnChanges{
+  rating: number = 4;
+  starWidth: number;
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 86 / 5;
+  }
+}
