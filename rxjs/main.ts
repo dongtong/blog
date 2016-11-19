e7b311bd2a33e2d0f568fb456e1c052e690aa3df
djh <jinhu.tung@gmail.com>
2016-11-20 00:57:17 +0800

    [RxJS] the first Observable and Observer

--- rxjs/main.ts
+++ rxjs/main.ts
@@ -1,1 +1,21 @@
-alert("Hello RxJS")
+import { Observable, Observer } from 'rxjs';
+
+let numbers = [1,2,3];
+let dataSource = Observable.from(numbers);
+
+class MyObserver implements Observer<number> {
+
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.error(`error: ${e}`);
+    }
+
+    complete() {
+        console.log('complete')
+    }
+}
+
+dataSource.subscribe(new MyObserver());
