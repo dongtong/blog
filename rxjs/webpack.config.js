2ba0ebb54e74892bb8d57ad6e920b71536e8ad18
djh <jinhu.tung@gmail.com>
2016-11-20 00:24:54 +0800

    [RxJS] Install and set up environment

---
+++ rxjs/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './main',
+    output: {
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["", ".ts", ".js"]
+    }
+};
