3311ed78fc3dc74f03e311a48fb0979a92dcc866
dongtong <jinhu.tung@gmail.com>
2019-02-13 02:00:30 -0800

    add css loader

--- webpack/webpack4/src/index.js
+++ webpack/webpack4/src/index.js
@@ -1,8 +1,11 @@
 import { join } from 'lodash';
+import './css/main.css';
 
 const createDOMEl = () => {
   let dom = document.createElement('div');
   dom.innerHTML = join(['Hello', ' ', 'Webpack 4!'], '');
+  //dom.className = 'red';
+  dom.classList.add('red');
   return dom;
 }
 
--- webpack/webpack4/webpack.config.js
+++ webpack/webpack4/webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   }
 }
